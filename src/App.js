ac8f1f6c0825b536154bfb09b4faf4a441f6f1da
mi929lee <mi929lee@gmail.com>
2021-07-30 17:02:16 -0700

    added firebase to load list data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WishList from "./components/WishList";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import InventoryList from "./components/InventoryList";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAcGOj84HiCQR-Qz_KijwkrPnKrO7IjJgM",
+  authDomain: "food-tracker-4c162.firebaseapp.com",
+  projectId: "food-tracker-4c162",
+  storageBucket: "food-tracker-4c162.appspot.com",
+  messagingSenderId: "153863168250",
+  appId: "1:153863168250:web:bf889a5ebe32bb34e66943",
+  measurementId: "G-6XRED8YZPJ",
+});
+// const firestore = firebase.firestore();
+// const temp = firestore.collection("InventoryList");
+
+// temp.get().then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     console.log(doc.id);
+//     console.log(doc.data());
+//   });
+// });
+
+// temp.add({
+//   title: "avocados",
+//   quantity: "4",
+//   tags: "fruit",
+//   date: "2021-03-14",
+//   location: "garage",
+//   description: "hello",
+// })
+// .then((docRef) => {
+//   console.log('document written with ID ', docRef.id);
+// })
+// .catch((error) => {
+//   console.error('error adding document ', error);
+// });
+
+// temp.doc('c645e20R2OJb5MR0FV3o').delete().then(() => {
+//   console.log('document successfully deleted!');
+// }).catch((error) => {
+//   console.error('error removing document', error);
+// })
 
 const App = () => {
   const [inventoryList, setInventoryList] = useState([
@@ -59,6 +102,44 @@ const App = () => {
       location: "safeway",
     },
   ]);
+
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection("InventoryList")
+      .get()
+      .then((querySnapshot) => {
+        const list = [];
+        // const date = doc.data().date.toDate().toDateString();
+        querySnapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            ...doc.data(),
+            date: doc.data().date.toDate().toDateString(),
+          });
+        });
+        setInventoryList(list);
+      });
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection("WishList")
+      .get()
+      .then((querySnapshot) => {
+        const list = [];
+        querySnapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            ...doc.data()
+          });
+        });
+        setWishList(list);
+      });
+    return unsubscribe;
+  }, []);
 
   const addInventoryItem = (row) => setInventoryList([...inventoryList, row]);
   const addWishItem = (row) => setWishList([...wishList, row]);
